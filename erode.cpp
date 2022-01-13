a707c9516691b860261fba2a8a165683cf16b6e8
zy-0617 <1324960770@qq.com>
2022-01-13 16:02:32 +0800

    update2

--- erode.cpp
+++ erode.cpp
@@ -2,14 +2,19 @@
 #include<opencv2/imgproc/imgproc.hpp>
 using namespace cv;
 
-int main() {
+int main()
+{
     Mat srcImage=imread("/home/zy/CLionProjects/test2/1.jpg");
+
     imshow("【原图】腐蚀操作",srcImage);
     Mat element=getStructuringElement(MORPH_RECT,Size(15,15));
+
     Mat dstImage;
     erode(srcImage,dstImage,element);
-    imshow("【效果图】腐蚀操作",dstImage);
-    waitKey(0);
 
+    namedWindow("【效果图】腐蚀操作",WINDOW_NORMAL);
+    imshow("【效果图】腐蚀操作",dstImage);
+
+    waitKey(0);
     return 0;
 }
