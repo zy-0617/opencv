219e7fd38b0202c632306c1e9cdd3e08cb1796a1
zy-0617 <1324960770@qq.com>
2022-01-13 16:25:32 +0800

    update3

--- split.cpp
+++ split.cpp
@@ -12,21 +12,9 @@ int main()
     std::vector<cv::Mat> channels;
     cv::Mat color_channel;
     cv::split(src, channels);
-    /*if (enemy_color == 0) {
-        color_channel = channels[0];
-    } else if (enemy_color == 1) {
-        color_channel = channels[2];
-    }*/
     color_channel = channels[2];
     int light_threshold=220;
-    /*if(enemy_color == 0){
-        light_threshold = 220;
-    }else{
-        light_threshold = 200;
-    }*/
-
     cv::threshold(color_channel,color_channel,light_threshold, 255,cv::THRESH_BINARY); // 二值化对应通道
-
     cv::imshow("picture",color_channel);
     cv::waitKey(0);
 }
