3186c6ba6473c932f74ac1246ef7f3d10c258299
zy-0617 <1324960770@qq.com>
2022-01-14 08:52:08 +0800

    resize

--- split.cpp
+++ split.cpp
@@ -6,15 +6,19 @@ using namespace cv;
 using namespace std;
 
 uint8_t enemy_color=1;
+
 int main()
 {
     Mat src=imread("../8.jpeg");
+
     std::vector<cv::Mat> channels;
     cv::Mat color_channel;
     cv::split(src, channels);
+
     color_channel = channels[2];
     int light_threshold=220;
     cv::threshold(color_channel,color_channel,light_threshold, 255,cv::THRESH_BINARY); // 二值化对应通道
+
     cv::imshow("picture",color_channel);
     cv::waitKey(0);
 }
